09ee8f0cb43e155676fb778e5613d4083795c5e4
Eduardo Schneiders <eduardoschneiders@Eduardos-MacBook-Pro.local>
2023-02-15 11:24:42 -0300

    Work on output_score of the game

--- lib/tennis_game_scorer.rb
+++ lib/tennis_game_scorer.rb
@@ -3,18 +3,79 @@
 require 'byebug'
 
 class TennisGameScorer
-  attr_accessor :game
+  attr_accessor :match_points, :points, :games, :sets
 
-  def play(game)
-    @game = game
+  def play(match_points)
+    @match_points = match_points
     validate_input!
+    set_player_points
   end
 
-  def output_score; end
+  def output_score
+    "#{sets_status}, #{games_status}, #{current_game_status}"
+  end
 
   private
 
   def validate_input!
-    raise 'Invalid input' unless game.uniq.all? { |e| [0, 1].include?(e) }
+    raise 'Invalid input' unless match_points.uniq.all? { |e| [0, 1].include?(e) }
+  end
+
+  def set_player_points
+    @points = [0, 0]
+    @games = [0, 0]
+    @sets = [0, 0]
+
+    match_points.each do |point|
+      points[0] += 1 if point == 0
+      points[1] += 1 if point == 1
+
+      if deuce?
+
+      end
+      # @points = [0, 0] if deuce?
+
+      if points[0] == 4 || points[1] == 4 && two_points_difference? &&!deuce?
+        games[0] += 1 if points[0] == 4
+        games[1] += 1 if points[1] == 4
+        @points = [0, 0]
+      end
+
+      if games[0] == 6 || games[1] == 6
+        sets[0] += 1 if games[0] == 6
+        sets[1] += 1 if games[1] == 6
+
+        @games = [0, 0]
+        @points = [0, 0]
+      end
+    end
+  end
+
+  def two_points_difference?
+    (points[0] - points[1]).abs == 2
+  end
+
+  def deuce?
+    points[0] >= 3 && points[1] >= 3
+  end
+
+  def advantage?
+    deuce? && points[0] != points[1]
+  end
+
+  def current_game_status
+    return "current game: #{points[0] > points[1] ? 'A' : points[0]} - #{points[1] > points[0] ? 'A' : points[1]}" if advantage?
+    return 'current game: Deuce' if deuce?
+
+    "current game: #{points[0]} - #{points[1]}"
+  end
+
+  def games_status
+    game_score = ", game score: #{games[0]}-#{games[1]}" if games.sum > 0
+    "#{games.sum} games played#{game_score}"
+  end
+
+  def sets_status
+    "#{sets.sum} sets played"
   end
 end
--- specs/tennis_game_scorer_spec.rb
+++ specs/tennis_game_scorer_spec.rb
@@ -18,5 +18,69 @@ RSpec.describe TennisGameScorer do
   end
 
   describe '#output_score' do
+    subject(:output_score) { game_scorer.output_score}
+    let(:game_scorer) { described_class.new}
+    let(:game) { [1, 0] }
+
+    before do
+      game_scorer.play(game)
+    end
+
+    context 'all right' do
+      let(:game) { [0, 0] }
+
+      it 'returns the game status' do
+        expect(output_score).to eql('0 sets played, 0 games played, current game: 2 - 0')
+      end
+    end
+
+    context '' do
+      let(:game) { [0, 0, 1, 1] }
+
+      it 'returns the game status' do
+        expect(output_score).to eql('0 sets played, 0 games played, current game: 2 - 2')
+      end
+    end
+
+    context 'when Deuce' do
+      let(:game) { [0, 0, 1, 1, 0, 1] }
+
+      it 'returns the game status' do
+        expect(output_score).to eql('0 sets played, 0 games played, current game: Deuce')
+      end
+    end
+
+    context 'Advantage' do
+      let(:game) { [0, 0, 1, 1, 0, 1, 1] }
+
+      it 'returns the game status' do
+        expect(output_score).to eql('0 sets played, 0 games played, current game: 3 - A')
+      end
+    end
+
+    context 'when 1 game was completed' do
+      let(:game) { [0, 0, 0, 0, 0, 0, 1, 1] }
+
+      it 'returns the game status' do
+        expect(output_score).to eql('0 sets played, 1 games played, game score: 1-0, current game: 2 - 2')
+      end
+    end
+
+    context 'when 1 set was completed' do
+      let(:game) do
+        [
+          0, 0, 0, 0, # game 1
+          0, 0, 0, 0, # game 2
+          0, 0, 0, 0, # game 3
+          0, 0, 0, 0, # game 4
+          0, 0, 0, 0, # game 5
+          0, 0, 0, 0, # game 6
+        ]
+      end
+
+      it 'returns the game status' do
+        expect(output_score).to eql('1 sets played, 0 games played, current game: 0 - 0')
+      end
+    end
   end
 end
